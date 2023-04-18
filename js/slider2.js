cfb7758289cfc8b09c1ef00e02cc577e2886385b
Joelle Johnson <joelle.johnson@dcmail.ca>
2023-04-18 13:19:58 -0400

    Slider cleanup

--- js/slider2.js
+++ js/slider2.js
@@ -1,6 +1,11 @@
+// This is the JS file for the 1 App, 3 Modes Slider
+
 const tabs = document.querySelectorAll('.slider-tab');
 const slides = document.querySelectorAll('.slide');
 let currSlide = null;
+let prevSlide = null;
+let slideDelay = getComputedStyle(document.querySelector('.slide')).getPropertyValue('--_slide-transition');
+slideDelay = parseFloat(slideDelay);
 
 for (let i=0; i < tabs.length; i++) {
   tabs[i].addEventListener('click', tabSelect.bind(null, i), false);
@@ -9,10 +14,49 @@ for (let i=0; i < slides.length; i++) {
   slides[i].addEventListener('click', tabSelect.bind(null, i), false);
 }
 
+function collapseSlide(num) {
+  let animName;
+  switch (num) {
+    case 0:
+      animName = 'top-up'
+      break;
+    case 1:
+      animName = 'mid-in'
+      break;
+    case 2:
+      animName = 'btm-down'
+      break;
+  
+    default:
+      break;
+  }
+  slides[num].style.animationName = animName;
+}
+function expandSlide(num) {
+  let animName;
+  switch (num) {
+    case 0:
+      animName = 'top-down'
+      break;
+    case 1:
+      animName = 'mid-out'
+      break;
+    case 2:
+      animName = 'btm-up'
+      break;
+  
+    default:
+      break;
+  }
+  slides[num].style.animationName = animName;
+}
+
 function tabSelect(num) {
   if (num == currSlide) {
-    removeActiveTab();
+    removeActiveTab(num);
+    currSlide = prevSlide = null;
   } else {
+    prevSlide = currSlide;
     currSlide = num;
     setActiveTab(num);
   }
@@ -30,17 +74,20 @@ function checkActiveTab() {
 function setActiveTab(num) {
   const isTabActive = checkActiveTab();
   if (isTabActive) {
-    removeActiveTab();
+    removeActiveTab(prevSlide);
     setTimeout(() => {
       slides[num].classList.add('active');
       tabs[num].classList.add('active');
-    }, 400);
+      expandSlide(num);
+    }, slideDelay);
   } else {
     slides[num].classList.add('active');
     tabs[num].classList.add('active');
+    expandSlide(num);
   }
 }
-function removeActiveTab() {
+function removeActiveTab(num) {
+  collapseSlide(num);
   slides.forEach(slide => {
     slide.classList.remove('active');
   });
